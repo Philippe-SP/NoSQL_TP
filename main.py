51eaade7b1fbf6347b01d0e3cdfdde6305831234
philippe-p <134625964+Philippe-SP@users.noreply.github.com>
2025-01-28 16:38:28 +0100

    Reception des messages et ajout en bdd

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from pymongo import MongoClient
 import json
 import pymongo
+import paho.mqtt.client as mqtt
 
-# ----- Initialisation de la base données ----- #
-client = MongoClient("localhost", 27017)
-db = client["AutoDrive_Pinheiro"]
+# ----------------------------------------------- Initialisation de la base de donnée ------------------------------------------------- #
+mongodb_client = MongoClient("localhost", 27017)
+db = mongodb_client["AutoDrive_Pinheiro"]
+
+# Création de la collection timeseries
+timeseries_collection = db["timeseries"]
 
 # Création du schéma de validation
 company_schema = {
@@ -39,29 +43,63 @@ company_schema = {
             "bsonType": "string",
             "description": "N° de téléphone de l'entreprise"
         },
-        "Taxis": {
+        "taxis": {
             "bsonType": "array",
             "description": "Taxis de l'entreprise"
         },
     }
 }
 
-# Création des collection
+# Création de la collection company
 try:
     company_collection = db.create_collection("company", validator = {"$jsonSchema": company_schema})
     print("Collection 'company' créée avec succès avec un schéma de validation.")
+
+    # Ajout des données via le fichier "taxis.json"
+    with open("data/taxis.json") as f:
+        data_file = json.load(f)
+
+    try:
+        if isinstance(data_file, list):
+            result = company_collection.insert_many(data_file)
+            print(f"Insertion réussie : {len(result.inserted_ids)} documents ajoutés.")
+        else:
+            print("Les données ne sont pas au format attendu (liste d'objets JSON).")
+    # Erreur d'insertion
+    except pymongo.errors.BulkWriteError as e:
+        print("Erreur lors de l'insertion :", e.details)
+# Erreur de création de la collection
 except pymongo.errors.CollectionInvalid:
     print("La collection 'company' existe déjà.")
 
-# Ajout des données via le fichier "taxis.json"
-with open("data/taxis.json") as f:
-    data_file = json.load(f)
-
-try:
-    if isinstance(data_file, list):
-        result = company_collection.insert_many(data_file)
-        print(f"Insertion réussie : {len(result.inserted_ids)} documents ajoutés.")
+# ------------------------------------------------- Initialisation du server MQTT ------------------------------------------------------- #
+# Callback quand la connexion au broker est établie
+def on_connect(mqtt_client, userdata, flags, rc):
+    if rc == 0:
+        print("Connecté au broker MQTT !")
+        # Connection au topic
+        mqtt_client.subscribe("autonomous_taxis/gps")
     else:
-        print("Les données ne sont pas au format attendu (liste d'objets JSON).")
-except pymongo.errors.BulkWriteError as e:
-    print("Erreur lors de l'insertion :", e.details)
+        print(f"Erreur de connexion : {rc}")
+
+# Callback quand un message est reçu
+def on_message(mqtt_client, userdata, msg):
+    print(f"Message reçu sur le topic {msg.topic}: {msg.payload.decode()}")
+    
+    # Convertion du message binaire en chaîne de caractères
+    message_str = msg.payload.decode()
+
+    # Convertion de la chaîne de caractères en document JSON
+    message_json = json.loads(message_str)
+    timeseries_collection.insert_one(message_json)
+
+# Configuration du client MQTT
+mqtt_client = mqtt.Client()
+mqtt_client.on_connect = on_connect
+mqtt_client.on_message = on_message
+
+# Connexion au broker MQTT
+mqtt_client.connect("broker.hivemq.com", 1883, 60)
+
+# Boucle infinie pour attendre les messages
+mqtt_client.loop_forever()
